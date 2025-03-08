6baad8d259010e8e6b9e419ae790b95bcd3de0b9
Jingnan Gao <64891841+G-1nOnly@users.noreply.github.com>
2025-03-08 17:41:26 +0800

    Add NeRF Synthetic dataloader

--- data_loader/__init__.py
+++ data_loader/__init__.py
@@ -3,14 +3,17 @@ import os
 import numpy as np
 import einops
 import torch
+from torch.utils.data import DataLoader
 
 import radfoam
 
 from .colmap import COLMAPDataset
+from .blender import BlenderDataset
 
 
 dataset_dict = {
     "colmap": COLMAPDataset,
+    "blender": BlenderDataset,
 }
 
 
@@ -31,6 +34,56 @@ def get_up(c2ws):
     return global_up
 
 
+def get_dataloader(
+    dataset_args,
+    split,
+    rays_per_batch=65_536,
+    downsample=None,
+    shuffle_data=True,
+):
+    data_dir = os.path.join(dataset_args.data_path, dataset_args.scene)
+    dataset = dataset_dict[dataset_args.dataset]
+    if downsample is not None:
+        split_dataset = dataset(
+            data_dir, split=split, downsample=downsample, is_stack=True
+        )
+    else:
+        split_dataset = dataset(data_dir, split=split, is_stack=True)
+
+    if split == "train":
+        n_rays = np.prod(split_dataset.all_rays.shape[:-1])
+        inds = np.random.choice(n_rays, n_rays, replace=False)
+        split_dataset.all_rays = split_dataset.all_rays.reshape(n_rays, 6)[inds]
+        split_dataset.all_rgbs = split_dataset.all_rgbs.reshape(n_rays, 3)[inds]
+
+        rem = n_rays % rays_per_batch
+        split_dataset.all_rays = split_dataset.all_rays[: n_rays - rem]
+        split_dataset.all_rgbs = split_dataset.all_rgbs[: n_rays - rem]
+
+        split_dataset.all_rays = split_dataset.all_rays.reshape(
+            -1, rays_per_batch, 6
+        )
+        split_dataset.all_rgbs = split_dataset.all_rgbs.reshape(
+            -1, rays_per_batch, 3
+        )
+
+    dataloader = DataLoader(
+        split_dataset,
+        batch_size=1,
+        shuffle=shuffle_data,
+        num_workers=1,
+        pin_memory=True,
+        prefetch_factor=2,
+    )
+
+    if hasattr(split_dataset, "cameras") and split_dataset.cameras is not None:
+        cameras = split_dataset.cameras
+    else:
+        cameras = None
+
+    return dataloader, cameras
+
+
 class DataHandler:
     def __init__(self, dataset_args, rays_per_batch, device="cuda"):
         self.args = dataset_args
@@ -44,15 +97,15 @@ class DataHandler:
         dataset = dataset_dict[self.args.dataset]
         if downsample is not None:
             split_dataset = dataset(
-                data_dir, split=split, downsample=downsample
+                data_dir, split=split, downsample=downsample, is_stack=True
             )
         else:
-            split_dataset = dataset(data_dir, split=split)
+            split_dataset = dataset(data_dir, split=split, is_stack=True)
         self.img_wh = split_dataset.img_wh
-        self.fx = split_dataset.fx
-        self.fy = split_dataset.fy
         self.c2ws = split_dataset.poses
-        self.rays, self.rgbs = split_dataset.all_rays, split_dataset.all_rgbs
+        self.K = split_dataset.intrinsics
+        self.rays, self.rgbs, self.alphas = split_dataset.all_rays, split_dataset.all_rgbs, split_dataset.all_alphas
+        self.cameras = split_dataset.cameras
 
         self.viewer_up = get_up(self.c2ws)
         self.viewer_pos = self.c2ws[0, :3, 3]
@@ -76,6 +129,8 @@ class DataHandler:
                 self.train_rays = self.train_rays[:, :, w_inds, :]
                 self.train_rgbs = self.rgbs[:, h_inds, :, :]
                 self.train_rgbs = self.train_rgbs[:, :, w_inds, :]
+                self.train_alphas = self.alphas[:, h_inds, :, :]
+                self.train_alphas = self.train_alphas[:, :, w_inds,:]
 
                 self.train_rays = einops.rearrange(
                     self.train_rays,
@@ -85,6 +140,12 @@ class DataHandler:
                 )
                 self.train_rgbs = einops.rearrange(
                     self.train_rgbs,
+                    "n (x ph) (y pw) c -> (n x y) ph pw c",
+                    ph=self.patch_size,
+                    pw=self.patch_size,
+                )
+                self.train_alphas = einops.rearrange(
+                    self.train_alphas,
                     "n (x ph) (y pw) c -> (n x y) ph pw c",
                     ph=self.patch_size,
                     pw=self.patch_size,
@@ -99,6 +160,9 @@ class DataHandler:
                     self.rgbs, "n h w c -> (n h w) c"
                 )
 
+                self.train_alphas = einops.rearrange(
+                    self.alphas, "n h w c -> (n h w) c"
+                )
                 self.batch_size = self.rays_per_batch
 
     def get_iter(self):
@@ -108,9 +172,14 @@ class DataHandler:
         rgb_batch_fetcher = radfoam.BatchFetcher(
             self.train_rgbs, self.batch_size, shuffle=True
         )
+        
+        alphas_batch_fetcher = radfoam.BatchFetcher(
+            self.train_alphas, self.batch_size, shuffle=True
+        )
 
         while True:
             ray_batch = ray_batch_fetcher.next()
             rgb_batch = rgb_batch_fetcher.next()
+            alphas_batch = alphas_batch_fetcher.next()
 
-            yield ray_batch, rgb_batch
+            yield ray_batch, rgb_batch, alphas_batch
---
+++ data_loader/blender.py
@@ -0,0 +1,147 @@
+import os
+import numpy as np
+from PIL import Image
+from tqdm import tqdm
+import torch
+from torch.utils.data import Dataset
+from torchvision import transforms as T
+import json
+import math
+
+
+from .ray_utils import *
+
+
+class BlenderDataset(Dataset):
+    def __init__(
+        self, datadir, split="train", downsample=1, is_stack=False, N_vis=-1
+    ):
+        assert downsample in [1, 2, 4, 8]
+
+        self.root_dir = datadir
+        self.split = split
+        self.is_stack = is_stack
+        self.define_transforms()
+
+        self.points3D = None
+        self.points3D_color = None
+
+        with open(os.path.join(self.root_dir, f"transforms_{self.split}.json"), 'r') as f:
+            meta = json.load(f)
+        if 'w' in meta and 'h' in meta:
+            W, H = int(meta['w']), int(meta['h'])
+        else:
+            W, H = 800, 800
+
+        self.w, self.h = W, H
+
+        if downsample > 1:
+            w, h = w // downsample, h // downsample
+        
+        self.img_wh = (self.w, self.h)
+
+        self.near, self.far = 2.0, 6.0
+
+        self.focal = 0.5 * self.w / math.tan(0.5 * meta['camera_angle_x']) # scaled focal length
+
+        fx, fy, cx, cy = self.focal, self.focal, self.w // 2, self.h // 2
+        self.intrinsics = torch.tensor([[fx, 0, cx], [0, fy, cy], [0, 0, 1]])
+
+        # type_ = cam.camera_type
+
+        # if type_ == 0 or type_ == "SIMPLE_PINHOLE":
+        #     params = None
+        #     camtype = "perspective"
+
+        # elif type_ == 1 or type_ == "PINHOLE":
+        #     params = None
+        #     camtype = "perspective"
+
+        # else:
+        #     assert type_ > 1, "Only support pinhole camera model."
+
+        self.directions = get_ray_directions(
+            self.h, self.w, [self.intrinsics[0, 0], self.intrinsics[1, 1]])  # (h, w, 3)
+
+        self.poses = []
+        self.cameras = []
+        self.all_rays = []
+        self.all_rgbs = []
+        self.all_alphas = []
+        for i, frame in enumerate(meta['frames']):
+            c2w = torch.from_numpy(np.array(frame['transform_matrix'])[:3, :4]).to(torch.float32)
+            # use float32 in radfoam
+            c2w[:3, 1:3] *= -1
+            # change from OpenGL/Blender camera axes (Y up, Z back) to COLMAP (Y down, Z forward)
+            self.poses.append(c2w)
+
+            img_path = os.path.join(self.root_dir, f"{frame['file_path']}.png")
+            img = Image.open(img_path)
+            img = self.transform(img) # (h*w, 4) RGB
+            img = img.permute(1, 2, 0)
+            image_mask = img[:, :, 3]
+            img = img[:, :, :3] * img[:, :, 3:4] + (1 - img[:, :, 3:4])
+            # by default white background
+            img = img.view(-1, 3)
+            image_mask = image_mask.view(-1, 1)
+
+            self.all_rgbs += [img]
+            self.all_alphas += [image_mask]
+
+            rays_o, rays_d = get_rays(self.directions, c2w)
+            self.all_rays += [torch.cat([rays_o, rays_d], -1)]  # (h*w, 6)
+            self.cameras += [rays_o.view(-1, 3)[0]]
+
+        self.poses = torch.stack(self.poses)
+        self.cameras = torch.stack(self.cameras)
+        if not self.is_stack:
+            self.all_rays = torch.cat(self.all_rays, 0)
+            self.all_rgbs = torch.cat(self.all_rgbs, 0)
+            self.all_alphas = torch.cat(self.all_alphas, 0)
+
+        else:
+            self.all_rays = torch.stack(self.all_rays, 0)
+            assert (
+                self.all_rays[0].shape[-1] == 6
+            ), "Rays should have 6 as last dim"
+            self.all_rays = self.all_rays.reshape(-1, *self.img_wh[::-1], 6)
+
+            self.all_rgbs = torch.stack(self.all_rgbs, 0)
+            assert (
+                self.all_rgbs[0].shape[-1] == 3
+            ), "RGBs should have 3 as last dim"
+            self.all_rgbs = self.all_rgbs.reshape(-1, *self.img_wh[::-1], 3)
+
+            self.all_alphas = torch.stack(self.all_alphas, 0)
+            assert (
+                self.all_alphas[0].shape[-1] == 1
+            ), "Alphas should have 1 as last dim"
+            self.all_alphas = self.all_alphas.reshape(-1, *self.img_wh[::-1], 1)
+
+    def define_transforms(self):
+        self.transform = T.ToTensor()
+
+    def __len__(self):
+        return len(self.all_rgbs)
+
+    def __getitem__(self, idx):
+
+        if self.split == "train":  # use data in the buffers
+            sample = {
+                "rays": self.all_rays[idx],
+                "rgbs": self.all_rgbs[idx],
+                "alphas": self.all_alphas[idx],
+            }
+
+        else:  # create data for each image separately
+
+            img = self.all_rgbs[idx]
+            rays = self.all_rays[idx]
+            alphas = self.all_alphas[idx]
+
+            sample = {"rays": rays, "rgbs": img, "alphas": alphas}
+        return sample
+
+
+if __name__ == "__main__":
+    pass
